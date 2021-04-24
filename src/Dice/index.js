c76958ef5aa3df548888679389472c60236b68ea
Filip Jirsák <filip@jirsak.org>
2021-04-24 18:06:32 +0200

    doplnění komentářů

--- src/Dice/index.js
+++ src/Dice/index.js
@@ -8,20 +8,28 @@ export const Dice = ({ side }) => {
   const diceElm = document.createElement('div')
   diceElm.classList.add('dice')
 
+  //div element, který zobrazuje stranu kostky s číslem; číslo je určené třídou dice__side--X
   const sideElm = document.createElement('div')
   sideElm.classList.add('dice__side', `dice__side--${side}`)
   diceElm.appendChild(sideElm)
 
+  //tlačítko „hodit“
   const buttonElm = document.createElement('button')
   buttonElm.classList.add('btn', 'btn--small', 'roll-btn')
   buttonElm.textContent = 'hodit'
   buttonElm.addEventListener('click', () => {
+    //každá pořádná stránka musí mít animace, takže si trochu vylepšíme zadání a při hození kostkou nastavíme třídu dice--roll, která kostku mírně zmenší…
     sideElm.classList.add('dice--roll');
     setTimeout(() => {
+      //… a po 300 ms třídu zase odstraníme, takže se kostka vrátí zpět a pokračujeme změnou čísla
       sideElm.classList.remove('dice--roll');
+      
+      //vracíme se zpět k původnímu zadání
+      //vymazat všechny třídy dice__side--X – kostka zůstane prázdná…
       for (let i = 1; i <= 6; i++) {
         sideElm.classList.remove(`dice__side--${i}`)
       }
+      //… a nastavit novou třídu dice__side--X podle hozeného čísla – výsledek volání roll()
       sideElm.classList.add(`dice__side--${roll()}`)
     }, 300)
   })
