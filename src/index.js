9059aa349105670ba6b2f5fec0cabf32c74f075b
Filip Jirsák <filip@jirsak.org>
2021-04-24 18:07:51 +0200

    doplnění komentářů

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './style.css';
+//budeme popužívat komponentu Dice nadefinovanou v souboru Dice/index.js (index.js se může vynechat)
 import { Dice } from './Dice'
 
 const roll = () => {
   return Math.floor(Math.random() * 6) + 1;
 };
-
 
 const diceRowElm = document.querySelector('#dice-row');
 // Na stránku vložím 10 kostek
